9be341b71498021417f305e60f57fd0743dc7b7e
Adilson <sr.adilson@gmail.com>
2021-08-20 15:13:06 -0300

    Add Services Cors

--- MassaApi/Startup.cs
+++ MassaApi/Startup.cs
@@ -25,7 +25,7 @@ namespace MassaApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -36,6 +36,11 @@ namespace MassaApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(c => 
+                c.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
