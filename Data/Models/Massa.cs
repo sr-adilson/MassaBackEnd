7583dec4de04bd792e94c3ba9f0a015d66db8600
Adilson <sr.adilson@gmail.com>
2021-08-20 14:06:40 -0300

    Create Data, Base models and massa models

---
+++ Data/Models/Massa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Massa : Base
+    {
+        public string Tipo { get; set; }
+        public string Molho { get; set; }
+        public string Queijos { get; set; }
+    }
+}
